b29ca05f08a25dae068aae867c4bd585b3df0143
h-ohshiba@eiwa.com <h-ohshiba@eiwa.com>
2016-02-21 16:27:17 +0900

    Java004のレビュ指摘の対応

--- Practice_Ohshiba_H/src/Java004/ArrayTest.java
+++ Practice_Ohshiba_H/src/Java004/ArrayTest.java
@@ -1,12 +1,13 @@
-
-package Java004;
-
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-
 /**
  * Copyright 2015 EIS Co., Ltd. All rights reserved.
  */
+
+package Java004;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.math.BigDecimal;
 
 /**
  * @author （作成者：大柴仁志） <br />
@@ -22,12 +23,13 @@ class ArrayTest {
    * （説明：コンソールへのメッセージ表示、コンソール入力処理および計算処理の実行） <br />
    * 
    * @param args 起動時引数
+   * @throws Exception
    */
-  public static void main( String[] args ) {
+  public static void main( String[] args ) throws Exception {
     // コンソールの入力値
-    int inputint[] = new int[5];
-    // 計算結果
-    int result[];
+    BigDecimal inputint[] = new BigDecimal[5];
+    // 計算結果（最大値、合計値、平均値）
+    BigDecimal result[] = new BigDecimal[3];
 
     // 入力要求メッセージの表示
     System.out.println( "整数を5回入力してください。" );
@@ -45,9 +47,12 @@ class ArrayTest {
       System.out.println( "平均値は" + result[2] );
 
       // 例外処理
-    } catch ( Exception e ) {
+    } catch ( NumberFormatException e ) {
       // エラー時はメッセージを表示
       System.out.println( "整数ではありません。処理を終了します。" );
+    } catch ( IOException e ) {
+      // エラー時はメッセージを表示
+      System.out.println( "異常が発生しました。処理を終了します。" );
     }
   }
 
@@ -59,9 +64,9 @@ class ArrayTest {
    * @return コンソールの入力値
    * @throws Exception コンソール入力が整数ではない。
    */
-  private static int[] inputproc() throws Exception {
+  private static BigDecimal[] inputproc() throws Exception {
     // コンソールの入力値
-    int inputint[] = new int[5];
+    BigDecimal inputint[] = new BigDecimal[5];
 
     // 入力ストリームのインスタンスを仮で生成。
     InputStreamReader input = null;
@@ -76,7 +81,8 @@ class ArrayTest {
       // コンソール入力の1行を読み込み
       String inputstr = inputbr.readLine();
       // コンソール入力を整数に変換
-      inputint[i] = Integer.parseInt( inputstr );
+      // inputint[i] = BigDecimal.valueOf(Integer.parseInt( inputstr ));
+      inputint[i] = new BigDecimal( inputstr );
     }
 
     // Streamのクローズ
@@ -92,25 +98,26 @@ class ArrayTest {
    * @param inputint コンソールの入力値
    * @return 計算結果（最大値、合計値、平均値）
    */
-  private static int[] calcproc( int inputint[] ) {
+  private static BigDecimal[] calcproc( BigDecimal inputint[] ) {
     // 計算結果（最大値、合計値、平均値）
-    int result[] = new int[3];
+    BigDecimal result[] = new BigDecimal[3];
+    result[1] = BigDecimal.valueOf( 0 );
 
     for ( int i = 0; i < 5; i++ ) {
 
       // 最大値の判定
       if ( i == 0 ) {
         result[0] = inputint[i];
-      } else if ( result[0] < inputint[i] ) {
+      } else if ( result[0].compareTo( inputint[i] ) < 0 ) {
         result[0] = inputint[i];
       }
 
       // コンソール入力値を累計
-      result[1] += inputint[i];
+      result[1] = result[1].add( inputint[i] );
     }
 
     // 合計値から平均値を算出
-    result[2] = result[1] / 5;
+    result[2] = result[1].divide( new BigDecimal( "5" ), 3, BigDecimal.ROUND_HALF_UP );
 
     return result;
   }
